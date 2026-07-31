83fba6a9e7c456aa94def5491e0d65721ffbee33
kkljx-jiandan <61830677+kkljx-jiandan@users.noreply.github.com>
2026-07-31 12:24:57 +0800

    Update inference_iterative_v2.py

--- project/Pangu/inference_iterative_v2.py
+++ project/Pangu/inference_iterative_v2.py
@@ -20,21 +20,21 @@ final_result_dir = '/home/mw/temp/'
 temp_dir = '/home/mw/temp/'
 
 
-#time_list = pd.date_range(start='2023-07-23 09:00:00', end='2023-07-31 06:00:00', freq='3h')
+#time_list = pd.date_range(start='2026-07-06 06:00:00', end='2026-07-19 06:00:00', freq='3h')
 
 # The date and time of the initial field
 date_time = datetime(
-    year=2023, 
+    year=2026, 
     month=7, 
-    day=23,
+    day=6,
     hour=6,
     minute=0)
 
 # The date and time of the final approaches
 date_time_final = datetime(
-    year=2023, 
+    year=2026, 
     month=7, 
-    day=29,
+    day=19,
     hour=6,
     minute=0)
 
